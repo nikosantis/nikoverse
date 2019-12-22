7d127d57d68b59c4b191d59efe8231d5038fb6cc
Nikolas Santis <nikosantis@gmail.com>
2019-12-22 12:43:52 -0300

    setup server.js

---
+++ nikoverse-mqtt/server.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const debug = require('debug')('nikoverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+
+const backend = {
+  type: 'redis',
+  redis,
+  return_buffers: true
+}
+
+const settings = {
+  port: 1883,
+  backend
+}
+
+const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', () => {
+  console.log(`${chalk.green('[nikoverse-mqtt]')} server is running`)
+})
+
+server.on('error', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
