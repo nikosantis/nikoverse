875060dc8301fcd98f87363cfdec0c0862ce729d
Nikolas Santis <nikosantis@gmail.com>
2019-12-22 13:26:53 -0300

    store agent info and report agent connected

--- nikoverse-mqtt/server.js
+++ nikoverse-mqtt/server.js
@@ -6,6 +6,8 @@ const redis = require('redis')
 const chalk = require('chalk')
 const db = require('nikoverse-db')
 
+const { parsePayload } = require('./utils')
+
 const backend = {
   type: 'redis',
   redis,
@@ -27,19 +29,65 @@ const config = {
 }
 
 const server = new mosca.Server(settings)
+const clients = new Map()
 
 let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Client Disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message': {
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+    }
+      break
+  }
+
   debug(`Payload: ${packet.payload}`)
 })
 
@@ -59,6 +107,11 @@ function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
