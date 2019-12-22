44657f3e8fba6378fae0e6036f177cd5eee03558
Nikolas Santis <nikosantis@gmail.com>
2019-12-22 17:18:04 -0300

    timer

--- nikoverse-agent/index.js
+++ nikoverse-agent/index.js
@@ -1,24 +1,53 @@
 'use strict'
 
+const debug = require('debug')('nikoverse:agent')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
 const EventEmitter = require('events')
+
+const options = {
+  name: 'untitled',
+  username: 'platzi',
+  interval: 5000,
+  mqtt: {
+    host: 'mqtt://localhost'
+  }
+}
 
 class NikoverseAgent extends EventEmitter {
   constructor(opts) {
     super()
 
-    this._options = opts
+    this._options = defaults(opts, options)
+    this._client = null
+
     this._started = false
     this._timer = null
   }
 
   connect() {
     if (!this._started) {
-      this._started = true
-      this.emit('connected')
       const opts = this._options
-      this._timer = setInterval(() => {
-        this.emit('agent/message', 'this is a message')
-      }, opts.interval)
+      this._client = mqtt.connect(opts.mqtt.host)
+      this._started = true
+
+      this._client.subscribe('agent/message')
+      this._client.subscribe('agent/connected')
+      this._client.subscribe('agent/disconnected')
+
+      this._client.on('connect', () => {
+        this.emit('connected')
+
+        this._timer = setInterval(() => {
+          this.emit('agent/message', 'this is a message')
+        }, opts.interval)
+      })
+
+      this._client.on('message', (topic, payload) => {
+
+      })
+
+      this._client.on('error', () => this.disconnect())
     }
   }
 
