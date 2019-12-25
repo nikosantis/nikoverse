d27b5b09176c579e30239acd063d166c54836937
Nikolas Santis <nikosantis@gmail.com>
2019-12-25 20:27:54 -0300

    proxy routes API

---
+++ nikoverse-web/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTc3MzE1OTU3fQ.AIZKxyFi62ojzig1NE_S-n6QpT6Q88t5KbtzVN7RM9M'
+}
--- nikoverse-web/proxy.js
+++ nikoverse-web/proxy.js
@@ -1,2 +1,34 @@
 'use strict'
 
+const express = require('express')
+const asyncify = require('express-asyncify')
+const request = require('request-promise-native')
+
+const { endpoint, apiToken } = require('./config')
+const api = asyncify(express.Router())
+
+api.get('/agents', async (req, res, next) => {
+  const options = {
+    method: 'GET',
+    url: `${endopint}/api/agents`,
+    headers: {
+      'Authorization': `Bearer ${apiToken}`
+    },
+    json: true
+  }
+
+  let result
+  try {
+    result = await request(options)
+  } catch (e) {
+    return next(e)
+  }
+
+  res.send(result)
+})
+
+api.get('/agent/:uuid', (req, res) => {})
+
+api.get('/metrics/:uuid', (req, res) => {})
+
+api.get('/metrics/:uuid/:type', (req, res) => {})
