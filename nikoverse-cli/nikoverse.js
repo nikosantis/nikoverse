6975b2a0e5b41da771b262a57f44688b8ccf67f5
Nikolas Santis <nikosantis@gmail.com>
2019-12-26 15:11:39 -0300

    agent monitoring 2

--- nikoverse-cli/nikoverse.js
+++ nikoverse-cli/nikoverse.js
@@ -6,6 +6,7 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const moment = require('moment')
 const NikoverseAgent = require('nikoverse-agent')
 
 const agent = new NikoverseAgent()
@@ -42,6 +43,49 @@ agent.on('agent/connected', payload => {
   renderData()
 })
 
+agent.on('agent/disconnected', payload => {
+  const { uuid } = payload.agent
+
+  if (agents.has(uuid)) {
+    agents.delete(uuid)
+    agentMetrics.delete(uuid)
+  }
+
+  renderData()
+})
+
+agent.on('agent/message', payload => {
+  const { uuid } = payload.agent
+  const { timestamp } = payload
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  const metrics = agentMetrics.get(uuid)
+
+  payload.metrics.forEach(m => {
+    const { type, value } = m
+
+    if (!Array.isArray(metrics[type])) {
+      metrics[type] = []
+    }
+
+    const length = metrics[type].length
+    if (length >= 20) {
+      metrics[type].shift()
+    }
+
+    metrics[type].push({
+      value,
+      timestamp: moment(timestamp).format('HH:mm:ss')
+    })
+  })
+
+  renderData()
+})
+
 function renderData () {
   const treeData = {}
 
@@ -52,6 +96,19 @@ function renderData () {
       agent: true,
       children: {}
     }
+
+    const metrics = agentMetrics.get(uuid)
+
+    Object.keys(metrics).forEach(type => {
+      const metric = {
+        uuid,
+        type,
+        metric: true
+      }
+
+      const metricName = ` ${type}`
+      treeData[title].children[metricName] = metric
+    })
   }
 
   tree.setData({
@@ -61,9 +118,11 @@ function renderData () {
   screen.render()
 }
 
+
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
 agent.connect()
+tree.focus()
 screen.render()
