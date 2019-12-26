d717e9c12e3974dff5f161d9f17408b5e47c8f48
Nikolas Santis <nikosantis@gmail.com>
2019-12-26 14:53:00 -0300

    agent monitoring 1

--- nikoverse-cli/nikoverse.js
+++ nikoverse-cli/nikoverse.js
@@ -6,7 +6,9 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const NikoverseAgent = require('nikoverse-agent')
 
+const agent = new NikoverseAgent()
 const screen = blessed.screen()
 
 const grid = new contrib.grid({
@@ -14,6 +16,9 @@ const grid = new contrib.grid({
   cols: 4,
   screen
 })
+
+const agents = new Map()
+const agentMetrics = new Map()
 
 const tree = grid.set(0, 0, 1, 1, contrib.tree, {
   label: 'Connected Agents'
@@ -26,8 +31,39 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+function renderData () {
+  const treeData = {}
+
+  for (let [ uuid, val ] of agents) {
+    const title = `${val.name} - ${val.pid}`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      children: {}
+    }
+  }
+
+  tree.setData({
+    extended: true,
+    children: treeData
+  })
+  screen.render()
+}
+
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
 screen.render()
