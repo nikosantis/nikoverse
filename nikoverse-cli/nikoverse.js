7213f941b744b82902d649ebe0ff2d7345995c67
Nikolas Santis <nikosantis@gmail.com>
2019-12-26 10:28:44 -0300

    Initial CLI

---
+++ nikoverse-cli/nikoverse.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const args = require('args')
+
+args
+  .option('port', 'The port on which the app will be running', 3000)
+  .option('reload', 'Enable/disable livereloading')
+  .command('serve', 'Serve your static site', ['s'])
+
+const flags = args.parse(process.argv)
