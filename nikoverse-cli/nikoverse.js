d66ee23a43730ee8cce64601ab849f548adcaafa
Nikolas Santis <nikosantis@gmail.com>
2019-12-26 11:04:43 -0300

    basic layout with blessed contrib

--- nikoverse-cli/nikoverse.js
+++ nikoverse-cli/nikoverse.js
@@ -2,11 +2,32 @@
 
 'use strict'
 
-const args = require('args')
+/* eslint new-cap: "off" */
 
-args
-  .option('port', 'The port on which the app will be running', 3000)
-  .option('reload', 'Enable/disable livereloading')
-  .command('serve', 'Serve your static site', ['s'])
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
 
-const flags = args.parse(process.argv)
+const screen = blessed.screen()
+
+const grid = new contrib.grid({
+  rows: 1,
+  cols: 4,
+  screen
+})
+
+const tree = grid.set(0, 0, 1, 1, contrib.tree, {
+  label: 'Connected Agents'
+})
+
+const line = grid.set(0, 1, 1, 3, contrib.line, {
+  label: 'Metric',
+  showLegend: true,
+  minY: 0,
+  xPadding: 5
+})
+
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+  process.exit(0)
+})
+
+screen.render()
