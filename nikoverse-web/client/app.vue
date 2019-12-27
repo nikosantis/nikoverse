0e9913081e106c2d2ab45eeb89129b9915f10607
Nikolas Santis <nikosantis@gmail.com>
2019-12-27 16:26:54 -0300

    pre production 2

--- nikoverse-web/client/app.vue
+++ nikoverse-web/client/app.vue
@@ -21,6 +21,7 @@
 <script>
   const request = require('request-promise-native')
   const io = require('socket.io-client')
+  const { serverHost } = require('../config')
   const socket = io()
 
   module.exports = {
@@ -38,7 +39,7 @@
       async initialize () {
         const options = {
           method: 'GET',
-          url: 'http://localhost:8080/agents',
+          url: `${serverHost}/agents`,
           json: true
         }
 
--- nikoverse-web/config.js
+++ nikoverse-web/config.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  serverHosts: process.env.SERVER_HOST || 'http://localhost:8080',
   apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTc3MzE1OTU3fQ.AIZKxyFi62ojzig1NE_S-n6QpT6Q88t5KbtzVN7RM9M'
 }
